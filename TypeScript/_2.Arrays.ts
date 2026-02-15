4d1e53c5d383f9403cc205d0289784303cf2c913
Rohit Kumar <Mr.developer2003@gmail.com>
2026-02-15 16:34:03 +0530

    Arrays in TS

---
+++ TypeScript/_2.Arrays.ts
@@ -0,0 +1,10 @@
+
+
+let getGreet:string[] =['Holla','Hey','Namaste'];
+console.log(getGreet);
+
+let getCount:number[] = [1,2,3,4,5,6,7,8,9,10];
+console.log(getCount);
+
+let StatusCode:number[] = [200,201,400,404,500];
+console.log(StatusCode);
